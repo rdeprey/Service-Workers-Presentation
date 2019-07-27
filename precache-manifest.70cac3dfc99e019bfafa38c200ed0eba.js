98ade084c72107d5b12308dfcd36660e80974078
Rebecca Deprey <rdeprey@gmail.com>
2019-07-27 12:27:41 -0400

    Updates

---
+++ precache-manifest.70cac3dfc99e019bfafa38c200ed0eba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d850c44f938e69222858",
+    "url": "/Service-Workers-Presentation/static/js/runtime~main.d850c44f.js"
+  },
+  {
+    "revision": "1ab07ee9ffcd63528302",
+    "url": "/Service-Workers-Presentation/static/js/main.1ab07ee9.chunk.js"
+  },
+  {
+    "revision": "cff928c49b526dc82604",
+    "url": "/Service-Workers-Presentation/static/js/1.cff928c4.chunk.js"
+  },
+  {
+    "revision": "1ab07ee9ffcd63528302",
+    "url": "/Service-Workers-Presentation/static/css/main.ba159a2d.chunk.css"
+  },
+  {
+    "revision": "cff928c49b526dc82604",
+    "url": "/Service-Workers-Presentation/static/css/1.1642938c.chunk.css"
+  },
+  {
+    "revision": "8bec1847541c997c8c28f089f7620c78",
+    "url": "/Service-Workers-Presentation/index.html"
+  }
+];
